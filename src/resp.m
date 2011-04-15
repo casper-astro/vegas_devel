748f5b7946f92bc5cc8c45924a0670bf5a997409
Mark Wagner <mwagner@otto.eecs.berkeley.edu>
2011-04-15 08:43:10 -0700

    Added comments to resp.m

--- src/resp.m
+++ src/resp.m
@@ -1,5 +1,11 @@
+%script to readout ToWorkspace Struct
+
 adc_vals_in=adc_input.signals.values(1:end);
 
 adc_vals_out=adc_dump.signals.values(1:end,1:end);
+
+%dimensions
+%number of elements
+numel(adc_vals_out)
 
 
