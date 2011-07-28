e8cb5cd9cbaef28b5a1d0931018a05aa3724d876
Mark Wagner <mwagner@otto.eecs.berkeley.edu>
2011-07-28 13:03:00 -0700

    Minor modifications.

--- lib/gbt_library/packetizer_init.m
+++ lib/gbt_library/packetizer_init.m
@@ -7,11 +7,11 @@ disp('Running mask script for block: packetizer')
 check_mask_type(blk, 'packetizer');
 munge_block(blk, varargin{:});
 
-sout_width = get_var('sout_width', 'defaults', defaults, varargin{:});
-pin_width = get_var('pin_width', 'defaults', defaults, varargin{:});
-fifo_depth = get_var('fifo_depth', 'defaults', defaults, varargin{:});
-total_words = get_var('total_words', 'defaults', defaults, varargin{:});
-words_per_cycle = pin_width/sout_width;
+sout_width = get_var('sout_width', 'defaults', defaults, varargin{:})
+pin_width = get_var('pin_width', 'defaults', defaults, varargin{:})
+fifo_depth = get_var('fifo_depth', 'defaults', defaults, varargin{:})
+total_words = get_var('total_words', 'defaults', defaults, varargin{:})
+words_per_cycle = pin_width/sout_width
 
 if ~(mod(pin_width, sout_width) == 0)
     errordlg('This block currently only supports integer word serialization')
@@ -23,6 +23,8 @@ end
 set_param([blk, '/packetizer_fsm'], 'defparams', ['{''data_per_word'',', num2str(words_per_cycle-1) ', ''n_words'', ' num2str(total_words-1), ' }']);
 
 % data_counter/shift_counter width
+num2str( ceil(log2(words_per_cycle-1))+1 )
+
 set_param([blk, '/shift_counter'], 'n_bits', num2str( ceil(log2(words_per_cycle-1))+1 ) );
 set_param([blk, '/data_counter'], 'n_bits', num2str( ceil(log2(total_words-1))+1) );
 
