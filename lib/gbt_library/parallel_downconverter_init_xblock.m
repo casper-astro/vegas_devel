4f03d43cfc7d21ff72626133f73091a1a8571315
Hong Chen <chenhong@intelr900-1.EECS.Berkeley.EDU>
2011-09-28 01:12:57 -0700

    continue working on CIC and related blocks. Generalized parallel downconverter so it support arbitrary number of inputs and arbitrary dec_rate. Test pending

--- lib/gbt_library/parallel_downconverter_init_xblock.m
+++ lib/gbt_library/parallel_downconverter_init_xblock.m
@@ -29,69 +29,129 @@ defaults = {'n_inputs', 4, 'dec_rate', 3};
 n_inputs = get_var('n_inputs', 'defaults', defaults, varargin{:});
 dec_rate = get_var('dec_rate', 'defaults', defaults, varargin{:});
 
-if mod(n_inputs,2)==1
-    disp('not supported yet');
-    return;
-end
 
-if mod(dec_rate,2) == 0
-    disp('not supported yet');
-    return;
-end
-
-
-%% finding the amount of delays
-% not the most elegant way, but it works for now
-max_delay = ceil((n_inputs-1)*dec_rate/n_inputs)-1;
-delay_values = cell(1,n_inputs);
-in2out_map =cell(1,n_inputs);
-for i =1:n_inputs
-    for j=0:dec_rate-1
-        test = n_inputs*j+i;
-        if mod(test-1,dec_rate) == 0
-            delay_values{i} = max_delay - j;
-            in2out_map{i} = ceil(test/dec_rate);
-            break;
-        end
-    end
-end
-
-delay_values
-in2out_map
 
 sync_in = xInport('sync_in');
 inports = cell(1,n_inputs);
 
 sync_out = xOutport('sync_out');
 outports = cell(1,n_inputs);
-delay_blks = cell(1,n_inputs);
-delay_outs = cell(1,n_inputs);
-downsample_blks = cell(1,n_inputs);
 for i =1:n_inputs   
     inports{i} = xInport(['In',num2str(i)]);
     outports{i} = xOutport(['Out',num2str(i)]);
 end
-for i =1:n_inputs
-    delay_outs{i} = xSignal(['dO',num2str(i)]);
-    delay_blks{i} = xBlock(struct('source','Delay','name', ['delay',num2str(i)]), ...
-                              struct('latency', delay_values{i}), ...   
-                              {inports{i}}, ...
-                              {delay_outs{i}});
-    downsample_blks{i} = xBlock(struct('source', 'xbsBasic_r4/Down Sample', 'name', ['Down_sample',num2str(i)]), ...
-                               struct('sample_ratio',dec_rate, ...
-                                        'sample_phase','Last Value of Frame  (most efficient)', ...
-                                      'latency', 1), ...
-                          {delay_outs{i}}, ...
-                          {outports{in2out_map{i}}});
+
+if gcd(n_inputs, dec_rate) ~= 1
+    % when their greatest common factor is not 1
+    % this implementation is a very rough one
+    % should be working but not efficient
+    factor = gcd(n_inputs, dec_rate);
+    for i =1:n_inputs
+        if mod(i, factor) ~= 1
+            xBlock(struct('source', 'Terminator', 'name', ['terminator', num2str(i)]),...
+                [], ...
+                {inports{i}}, {});
+        end
+    end
+    
+    %% finding the amount of delays
+    % not the most elegant way, but it works for now
+    max_delay = ceil((n_inputs-1)*dec_rate/n_inputs)-1;
+    delay_values = cell(1,n_inputs);
+    in2out_map =cell(1,n_inputs);
+    counter = 1;
+    for j=0:dec_rate-1   % columns
+        for i =1:factor:n_inputs   % rows
+            test = n_inputs*j+i;
+            if mod(test-1, dec_rate) == 0
+                test
+                delay_values{counter} = max_delay - j;
+                in2out_map{counter} = [i, j];
+                [delay_values{counter}, in2out_map{counter}]
+                counter = counter + 1;
+                %continue;
+            end
+        end
+    end
+    
+    delay_values
+    in2out_map{:}
+
+
+    delay_blks = cell(1,n_inputs);
+    delay_outs = cell(1,n_inputs);
+    downsample_blks = cell(1,n_inputs);
+
+    for i =1:n_inputs
+        delay_outs{i} = xSignal(['dO',num2str(i)]);
+        delay_blks{i} = xBlock(struct('source','Delay','name', ['delay',num2str(i)]), ...
+                                  struct('latency', delay_values{i}), ...   
+                                  {inports{in2out_map{i}(1)}}, ...  % take the row
+                                  {delay_outs{i}});
+        downsample_blks{i} = xBlock(struct('source', 'xbsBasic_r4/Down Sample', 'name', ['Down_sample',num2str(i)]), ...
+                                   struct('sample_ratio',dec_rate, ...
+                                            'sample_phase','Last Value of Frame  (most efficient)', ...
+                                          'latency', 1), ...
+                              {delay_outs{i}}, ...
+                              {outports{i}});
+    end
+
+    % take care of sync pulse
+    sync_delay = xBlock(struct('source','Delay','name', 'sync_delay'), ...
+                                  struct('latency', 1), ...   
+                                  {sync_in}, ...
+                                  {sync_out});
+    
+else
+    %% finding the amount of delays
+    % not the most elegant way, but it works for now
+    max_delay = ceil((n_inputs-1)*dec_rate/n_inputs)-1;
+    delay_values = cell(1,n_inputs);
+    in2out_map =cell(1,n_inputs);
+    for i =1:n_inputs  % rows
+        for j=0:dec_rate-1  % columns
+            test = n_inputs*j+i;
+            if mod(test-1,dec_rate) == 0
+                delay_values{i} = max_delay - j;
+                in2out_map{i} = ceil(test/dec_rate);
+                %continue;
+            end
+        end
+    end
+
+    delay_values
+    in2out_map
+
+
+    delay_blks = cell(1,n_inputs);
+    delay_outs = cell(1,n_inputs);
+    downsample_blks = cell(1,n_inputs);
+
+    for i =1:n_inputs
+        delay_outs{i} = xSignal(['dO',num2str(i)]);
+        delay_blks{i} = xBlock(struct('source','Delay','name', ['delay',num2str(i)]), ...
+                                  struct('latency', delay_values{i}), ...   
+                                  {inports{i}}, ...
+                                  {delay_outs{i}});
+        downsample_blks{i} = xBlock(struct('source', 'xbsBasic_r4/Down Sample', 'name', ['Down_sample',num2str(i)]), ...
+                                   struct('sample_ratio',dec_rate, ...
+                                            'sample_phase','Last Value of Frame  (most efficient)', ...
+                                          'latency', 1), ...
+                              {delay_outs{i}}, ...
+                              {outports{in2out_map{i}}});
+    end
+
+    % take care of sync pulse
+    sync_delay = xBlock(struct('source','Delay','name', 'sync_delay'), ...
+                                  struct('latency', 1), ...   
+                                  {sync_in}, ...
+                                  {sync_out});
+
 end
 
-% take care of sync pulse
-sync_delay = xBlock(struct('source','Delay','name', 'sync_delay'), ...
-                              struct('latency', 1), ...   
-                              {sync_in}, ...
-                              {sync_out});
-                          
 if ~isempty(blk) && ~strcmp(blk(1),'/')
     clean_blocks(blk);
+    fmtstr = sprintf('Decimation Rate = %d', dec_rate);
+    set_param(blk, 'AttributesFormatString', fmtstr);
 end
 end
