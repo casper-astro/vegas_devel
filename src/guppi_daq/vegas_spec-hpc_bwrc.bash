2ee7db9b5f94ae3535754162a9ddf3e550024d3d
Simon Scott <simonscott@berkeley.edu>
2012-01-16 06:08:24 -0500

    Updated initialization files and Makefile to use common variables for library paths. Changed GUPPI_DIR var to VEGAS_DIR var.

--- src/guppi_daq/vegas_spec-hpc_bwrc.bash
+++ src/guppi_daq/vegas_spec-hpc_bwrc.bash
@@ -2,20 +2,20 @@
 # Set environment variables for GUPPI, bash version
 # Lines that have been commented out are lines that were either modified
 # or removed by Simon Scott, to allow installation on otto.
-echo "This script is specific to spec-hpc-xx"
+echo "This script is specific to spec-hpc-xx at the BWRC"
 echo "Setting GUPPI, VEGAS_DIR, CUDA, PYSLALIB, VEGAS_INCL/BIN/LIB, PATH, PYTHONPATH and LD_LIBRARY_PATH for VEGAS..."
 
 # Note: user must set the VEGAS variable in their bash startup script
 
 export VEGAS_DIR=$VEGAS/guppi_daq
 
-export CUDA=/opt/local/cuda
+export CUDA=/opt/vegas/cuda
 
-export PYSLALIB=/opt/local/lib/python2.5/site-packages/pyslalib
+export PYSLALIB=/opt/vegas/lib/python2.5/site-packages/pyslalib
 
-#export VEGAS_INCL=/opt/local/include
-#export VEGAS_BIN=/opt/local/bin
-export VEGAS_LIB=/home/gbt7/llama64/lib
+export VEGAS_INCL=/opt/vegas/include
+export VEGAS_BIN=/opt/vegas/bin
+export VEGAS_LIB=/opt/vegas/lib
 export VEGAS_LIB_GCC=/usr/lib/gcc/x86_64-redhat-linux/3.4.6
 
 export PATH=$VEGAS_DIR/bin:$CUDA/bin:$VEGAS_BIN:$PATH
