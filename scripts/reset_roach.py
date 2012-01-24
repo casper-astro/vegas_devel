f4820bc6df446801d344bc37ebe38398d7ff28ce
Mark Wagner <mwagner@eecs.berkeley.edu>
2012-01-24 15:21:16 -0500

    Adding new vegas configuration script.

--- scripts/reset_roach.py
+++ scripts/reset_roach.py
@@ -1,9 +1,29 @@
 import corr,time
 
-roach = 'roach1'
+roach = 'roach03'
+fpga=corr.katcp_wrapper.FpgaClient(roach,7147)
 
-#rst counter
+tick = time.time()
+
+while time.time() - tick < 5:
+    if fpga.is_connected():
+	break 
+
 fpga.write_int('rst',1)
 fpga.write_int('rst',0)
 
+def reset():
+    fpga.write_int('sg_sync',0x12)
+
+    #rst counter
+    fpga.write_int('rst',1)
+    fpga.write_int('rst',0)
+
+    fpga.write_int('arm',0)
+    fpga.write_int('arm',1)
+    fpga.write_int('arm',0)
+    fpga.write_int('sg_sync',0x12)
+    fpga.write_int('sg_sync',0x11)
+
+reset()
 
