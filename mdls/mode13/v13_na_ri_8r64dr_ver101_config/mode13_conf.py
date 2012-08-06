df6d781cf915af782f961b75862e8a820e575460
Jayanth Chennamangalam <jayanthc@gmail.com>
2012-08-06 15:28:13 -0700

    Mode 13 model and config files.

---
+++ mdls/mode13/v13_na_ri_8r64dr_ver101_config/mode13_conf.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python2.6
+
+import corr,time,numpy,struct,sys
+
+bitstream = 'v13_na_ri_8r64dr_ver101_2012_Aug_06_1424.bof'
+#roach = 'roach03'
+roach = '192.168.40.70'
+
+#Decide where we're going to send the data, and from which addresses:
+#dest_ip  = 10*(2**24) + 145 #10.0.0.145
+dest_ip  = 10*(2**24) + 1*(2**8) + 146 #10.0.1.146
+port     = 60000
+#src_ip   = 10*(2**24) + 10  #10.0.0.10
+src_ip   = 10*(2**24) + 1*(2**8) + 10  #10.0.1.10
+mac_base = (2<<40) + (2<<32)
+gbe0     = 'gbe0';
+
+print('Connecting to server %s on port... '%(roach)),
+fpga = corr.katcp_wrapper.FpgaClient(roach)
+time.sleep(2)
+
+if fpga.is_connected():
+	print 'ok\n'	
+else:
+    print 'ERROR\n'
+
+print '------------------------'
+print 'Programming FPGA with %s...' %bitstream,
+fpga.progdev(bitstream)
+print 'ok\n'
+time.sleep(5)
+
+print '------------------------'
+print 'Setting the port 0 linkup :',
+#fpga.listdev()
+gbe0_link=bool(fpga.read_int(gbe0))
+print gbe0_link
+if not gbe0_link:
+   print 'There is no cable plugged into port0'
+print '------------------------'
+print 'Configuring receiver core...',   
+# have to do this manually for now
+#fpga.tap_start('tap0',gbe0,mac_base+src_ip,src_ip,port)
+print 'done'
+
+print '------------------------'
+print 'Setting-up packet core...',
+sys.stdout.flush()
+fpga.write_int('dest_ip',dest_ip)
+fpga.write_int('dest_port',port)
+
+#fpga.write_int('sg_sync', 0b10100)
+#time.sleep(1)
+#fpga.write_int('arm', 0)
+#fpga.write_int('arm', 1)
+#fpga.write_int('arm', 0)
+#time.sleep(1)
+#fpga.write_int('sg_sync', 0b10101)
+#fpga.write_int('sg_sync', 0b10100)
+print 'done'
+
