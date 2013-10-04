03ea8e6ac5850f70285cc5efda674aba5a8e3d32
gitj <gej2113@columbia.edu>
2013-10-04 10:08:25 -0400

    fix 32 bit wrap in utcdelta
    Top 8 bits of FPGA time counter were not being propagated beyond udp packet.

--- src/vegas_hpc/src/vegas_udp.c
+++ src/vegas_hpc/src/vegas_udp.c
@@ -149,8 +149,9 @@ int vegas_udp_recv(struct vegas_udp_params *p, struct vegas_udp_packet *b, char 
 				unsigned long pktnum = (tmcounter - 10) / 2048;
                 unsigned long status_bits = (unsigned long) (tempbuf[1] & 0x0F);
                 hdr[1*2+0] = hdr[1*2+0] | htonl((unsigned int) ((pktnum >> 32) & 0x00000000000000FF));
-	            hdr[1*2+1] = htonl((unsigned int) (pktnum & 0x00000000FFFFFFFF));
-	            hdr[5*2+1] = htonl((tmcounter - 10) & 0x000000FFFFFFFFFF);
+                hdr[1*2+1] = htonl((unsigned int) (pktnum & 0x00000000FFFFFFFF));
+                hdr[5*2+0] = hdr[5*2+0] | htonl(((tmcounter - 10)>>32) & 0x00000000000000FF);
+                hdr[5*2+1] = htonl((tmcounter - 10) & 0x000000FFFFFFFFFF);
                 hdr[7*2+1] = htonl((unsigned int) (status_bits & 0x0000000F));
 	            b->packet_size = 8192+72;
 
