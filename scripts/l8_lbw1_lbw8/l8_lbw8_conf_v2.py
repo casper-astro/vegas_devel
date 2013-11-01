2b07c68e534b483f0ef62eaf4ec13a99fba82c81
Hong Chen <chen_1992@berkeley.edu>
2013-10-31 21:30:48 -0700

    Added l8_utils.py by Glenn and l8_lbw8_conf_v2.py by Jayanth, needs to clean up

---
+++ scripts/l8_lbw1_lbw8/l8_lbw8_conf_v2.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python2.6
+
+from subprocess import Popen, PIPE, STDOUT
+import corr,time,numpy,struct,sys, socket
+from matplotlib.pyplot import * 
+execfile('mixercic_funcs.py')
+import l8_utils
+execfile('l8_debug.py')
+
+n_inputs = 4 # number of simultaneous inputs - should be 4 for final design
+bramlength = 10 # size of brams used in mixers (2^?)
+
+mode_sel = 0  # 0 for mode l8_lbw8
+t_sleep = 1
+n_subbands = 8
+lo_f = [15, 77, 91, 135, 296, 588, 710, 1247]
+lo_f_actual = lo_f
+brd_clk = 1500. # bandwidth, in MHz, aka brd_clk
+bw = brd_clk
+dec_rate = 128. # decimation rate
+#nchan = 8192. # number of points in fft
+
+#boffile='v13_16r128dr_ver113b_2013_Mar_21_0034.bof'
+#boffile='v13_16r64dr_ver114_2013_Apr_06_2235.bof'
+#boffile='v13_16r128dr_ver117_2013_Apr_12_0131.bof'
+#boffile='l8_ver115_2013_May_17_1027.bof'
+#boffile='l8_ver117_2013_May_25_1242.bof'
+
+boffile='l8_ver118_2013_Jun_01_1651.bof'
+#boffile='l8_ver106_2013_Aug_25_2033.bof'
+
+roach = 'vegasr2-1'
+
+dest_ip  = 10*(2**24) + 17*(2**16) + 51 #10.17.0.51
+src_ip   = 10*(2**24) + 17*(2**16) + 64 #10.17.0.64
+
+dest_port     = 60000
+fabric_port     = 60000
+
+mac_base = (2<<40) + (2<<32)
+
+#-------------------------------------------------------
+#
+#-------------------------------------------------------
+
+print('Connecting to server %s on port... '%(roach)),
+fpga = corr.katcp_wrapper.FpgaClient(roach)
+time.sleep(2)
+
+if fpga.is_connected():
+	print 'ok\n'	
+else:
+    print 'ERROR\n'
+
+print '------------------------'
+print 'Programming FPGA with %s...' % boffile,
+fpga.progdev(boffile)
+print 'ok\n'
+time.sleep(5)
+
+print '------------------------'
+print 'Setting the port 0 linkup :',
+
+gbe0_link=bool(fpga.read_int('gbe0'))
+print gbe0_link
+
+if not gbe0_link:
+   print 'There is no cable plugged into port0'
+
+print '------------------------'
+print 'Configuring receiver core...',   
+# have to do this manually for now
+fpga.tap_start('tap0','gbe0',mac_base+src_ip,src_ip,fabric_port)
+print 'done'
+
+print '------------------------'
+print 'Setting-up packet core...',
+sys.stdout.flush()
+fpga.write_int('dest_ip',dest_ip)
+fpga.write_int('dest_port',dest_port)
+
+set_mode(mode_sel)
+time.sleep(1)
+fpga.write_int('sg_sync', 0b10100)
+time.sleep(1)
+fpga.write_int('arm', 0)
+fpga.write_int('arm', 1)
+fpga.write_int('arm', 0)
+time.sleep(1)
+fpga.write_int('sg_sync', 0b10101)
+fpga.write_int('sg_sync', 0b10100)
+print 'done'
+
+#########################################
+
+
+for i in range(n_subbands):
+    #lo_f_actual[i], wave_tmp = lo_setup(lo_f[i], 's'+str(i))
+    lo_f_actual[i], wave_tmp = l8_utils.lo_setup(fpga, ('s%d' % i), lo_freq=lo_f[i], fs=2*bw)
+    time.sleep(t_sleep)
+    setgain(i, 2**12, 2**14)
+    time.sleep(t_sleep)
+
+print "Board Clock: ",fpga.est_brd_clk()
